a4e758ca93e90d9d1d521e812c292a67943e8dc6
bolo <bolo@autistici.org>
2019-04-19 17:43:35 +0200

    modified:   insertion_sort.py 	new file:   merge_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,6 +2,8 @@
 
 # Insertion Sort script
 # Best O(n) --> Average O(n^2) --> Worst O(n^2)
+import time
+import random
 
 def insertionSort(alist):
 	for index in range(1,len(alist)):
@@ -16,6 +18,12 @@ def insertionSort(alist):
 	return alist
 
 alist = [100, 25, 2, 8, -1, 44]
+# alist = random.sample(range(1, 10000), 1000)
+
 print "[*] Unsorted list %s" % (alist)
+start = time.time()
 nlist = insertionSort(alist)
-print "[*] Sorted list %s" % (nlist)	
+end = time.time()
+
+print "[*] Sorted list %s" % (nlist)
+print "[*] Script took %s seconds" % (end - start)
---
+++ merge_sort.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+# Merge Sort script
+# Best O(n log n) --> Average O(n log n) --> Worst O(n log n)
+import time
+import random
+
+
+def merge(left, right):
+	
+	left_index, right_index = 0, 0
+	result = []
+	while left_index < len(left) and right_index < len(right):
+		if left[left_index] < right[right_index]:
+			result.append(left[left_index])
+			left_index += 1
+		else:
+			result.append(right[right_index])
+			right_index += 1
+	result += left[left_index:]	
+	result += right[right_index:]
+	return result
+
+
+def merge_sort(array):
+	if len(array) <= 1:
+		return array
+
+	# divide array in half and merge sort recursively
+	half = len(array) // 2
+
+	left = merge_sort(array[half:])
+	right = merge_sort(array[:half])
+	return merge(left, right)
+
+array = [100, 25, 2, 8, -1, 44]
+# array = random.sample(range(1, 10000), 1000)
+print "[*] Unsorted list %s" % (array)
+
+start = time.time()
+narray = merge_sort(array)
+end = time.time()
+
+print "[*] Sorted list %s" % (narray)
+print "[*] Script took %s seconds" % (end - start)
